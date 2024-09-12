5a0d0f28df3a0e58b8b65f612e4f9e81d040c1f5
Aziret <85331267+beysheno@users.noreply.github.com>
2024-09-12 21:20:56 +0600

    Add files via upload
    add less1

---
+++ less1/app.js
@@ -0,0 +1,45 @@
+document.write("<h1> Hello, group 42 from app.js </h1>")
+document.body.style.color  = 'white'
+
+
+// commets
+
+
+console.log("Hello Baby");
+// alert('whats poppin')
+/*
+
+variables and data types
+
+1. String
+2. Number
+3. Boolean
+4. Underfined
+5. Null
+6. Object
+
+7. bigint
+8. symbol
+
+*/
+
+// var let const
+
+let countryName = " Kyrgyz Republic";
+let countryCode2 = 'KG';
+let countryCode3 = "KGZ";
+let countryFlag = "🇰🇬"
+ 
+console.log("Country Info:", countryName, countryCode2, countryCode3,countryFlag);
+
+let createdYear = 1991;
+let independenceYears = 33;
+let population = 6_542_451;
+let area = 520_450_452.87;
+let govDebt = -500_000_000_000;
+
+let hasArmy = true;
+let isEuropean = false;
+let hasMarineBoard = false;
+let isAsia = true;
+
