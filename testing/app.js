2e4101418736f831dedbc10f9b90825ad96367d2
Aziret <85331267+beysheno@users.noreply.github.com>
2024-09-28 21:22:39 +0600

    Update app.js

--- testing/app.js
+++ testing/app.js
@@ -7,8 +7,8 @@ function calculateTip() {
   if (!$amount.value || !$tipPercent.value) {
     $total.innerText = "";
   } else {
-    const amount = parseFloat(document.getElementById("amount").value);
-    const tipPercent = parseFloat(document.getElementById("tip-percent").value);
+    const amount = parseFloat($amount.value);
+    const tipPercent = parseFloat($tipPercent.value);
     const total = amount + (amount * tipPercent) / 100;
     $total.innerText = total.toFixed(2);
   }
